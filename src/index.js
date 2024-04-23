785d8e6dad09df7c0aeefc3aa8827e51579414d9
Lakshit Ladha <lakshitladha05@gmail.com>
2024-04-23 16:06:30 +0530

    Added notification ticket model

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser= require("body-parser");
+const cron= require("node-cron");
 
 const { PORT } = require("./config/serverconfig");
 
@@ -14,12 +15,15 @@ const setUpAndStartServer=()=>{
     app.listen(PORT, ()=>{
         console.log(`Server started at PORT: ${PORT}`);
 
-        sendBasicEmail(
-            "lakshitladha05@gmail.com",
-            "lakshitladha05@gmail.com",
-            "This is a testing email",
-            "hey, how are you, I hope you like this support"
-        );
+        // sendBasicEmail(
+        //     "lakshitladha05@gmail.com",
+        //     "lakshitladha05@gmail.com",
+        //     "This is a testing email",
+        //     "hey, how are you, I hope you like this support"
+        // );
+        cron.schedule('*/1 * * * *', () => {
+            console.log('running a task every two minutes');
+        });
     });
 }
 
